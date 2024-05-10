53e7c5e03215d890427bbe9eaca79da3ebad4974
Rajlaxmi Shrirame <96939792+rajlaxmi-shrirame@users.noreply.github.com>
2024-05-11 01:28:17 +0530

    Update A3-PrimsMST.cpp

--- LP2/A3-PrimsMST.cpp
+++ LP2/A3-PrimsMST.cpp
@@ -6,53 +6,60 @@
 
 using namespace std;
 
-
-struct Edge {
+struct Edge
+{
     int to;
     int weight;
 };
 
-vector<pair<int, int>> primMST(vector<vector<Edge>>& graph) {
-    int n = graph.size(); 
-    vector<int> minWeight(n, INT_MAX); 
+vector<pair<pair<int, int>, int>> primMST(vector<vector<Edge>> &graph)
+{
+    int n = graph.size();
+    vector<int> minWeight(n, INT_MAX);
     vector<int> parent(n, -1);
-    vector<bool> visited(n, false); 
-    
+    vector<bool> visited(n, false);
+
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    
+
     minWeight[0] = 0;
-    pq.push({0, 0}); 
-    
-    while (!pq.empty()) {
+    pq.push({0, 0});
+
+    while (!pq.empty())
+    {
         int u = pq.top().second;
+        int weight = pq.top().first; // Retrieve the weight
         pq.pop();
-        
+
         visited[u] = true;
-        
-        for (Edge& edge : graph[u]) {
+
+        for (Edge &edge : graph[u])
+        {
             int v = edge.to;
-            int weight = edge.weight;
-            
-            if (!visited[v] && weight < minWeight[v]) {
-                minWeight[v] = weight;
+            int edgeWeight = edge.weight; // Retrieve the weight of the edge
+
+            if (!visited[v] && edgeWeight < minWeight[v])
+            {
+                minWeight[v] = edgeWeight;
                 parent[v] = u;
                 pq.push({minWeight[v], v});
             }
         }
     }
-    
-    vector<pair<int, int>> MST;
-    for (int i = 1; i < n; ++i) {
-        MST.push_back({parent[i], i});
+
+    vector<pair<pair<int, int>, int>> MST; // Modify the return type to store edge weight
+    for (int i = 1; i < n; ++i)
+    {
+        MST.push_back({{parent[i], i}, minWeight[i]}); // Store both edge endpoints and weight
     }
-    
+
     return MST;
 }
 
-int main() {
-    int n = 5; 
+int main()
+{
+    int n = 5;
     vector<vector<Edge>> graph(n);
-    
+
     // Add edges
     graph[0].push_back({1, 2});
     graph[0].push_back({3, 1});
@@ -61,13 +68,14 @@ int main() {
     graph[1].push_back({4, 4});
     graph[2].push_back({4, 5});
     graph[3].push_back({4, 3});
-    
-    vector<pair<int, int>> MST = primMST(graph);
-    
-    cout << "Edges of the minimum spanning tree:" << endl;
-    for (auto edge : MST) {
-        cout << edge.first << " - " << edge.second << endl;
+
+    vector<pair<pair<int, int>, int>> MST = primMST(graph); // Modify the vector type
+
+    cout << "Edges of the minimum spanning tree with weights:" << endl;
+    for (auto edge : MST)
+    {
+        cout << edge.first.first << " - " << edge.first.second << " Weight: " << edge.second << endl; // Print edge weight
     }
-    
+
     return 0;
 }
